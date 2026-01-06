11eca848145a49198dfa341d4d134e49580da735
ac-Spark <csl426@aicreate360.com>
2026-01-06 09:33:21 +0800

    Remove SSR and disable Vite HMR

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { join, dirname, resolve } from "path";
+import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
 
@@ -7,4 +7,12 @@ const path = fileURLToPath(import.meta.url);
 export default {
   root: join(dirname(path), "client"),
   plugins: [react()],
+  server: {
+    hmr: false,
+    watch: null,
+  },
+  // 完全禁用 HMR client 注入
+  define: {
+    'import.meta.hot': 'undefined',
+  },
 };
